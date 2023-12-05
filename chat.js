c36885dff22f25b1b8f2e685235cbee41e5d06a7
Xondamir <abdulazizovxondamirxon@gmail.com>
2023-12-05 07:58:28 +0500

    sd

--- chat.js
+++ chat.js
@@ -1,47 +1,31 @@
 document.addEventListener("DOMContentLoaded", async () => {
   let logoutBtn = document.querySelector("#logout");
-  let ul = document.querySelector("ul")
+  let ul = document.querySelector("ul");
 
-    logoutBtn.addEventListener("click", async (e) => {
-      localStorage.removeItem("user-token");
-      window.location.replace("/");
-    });
+  logoutBtn.addEventListener("click", async (e) => {
+    localStorage.removeItem("user-token");
+    window.location.replace("/");
+  });
 
-
-  let li = document.createElement("li");
-  let img = document.createElement("img");
-  // img.setAttribute("src", )
- 
-
-  let titlee = document.createElement("h3");
-  let pricee = document.createElement("p")
-  let descriptionn =document.createElement("p")
-
-
-  li.append(img, titlee, pricee, descriptionn);
-
- 
   // ul.append(li)
 
-  
+  let { data: products } = await axios.get("https://fakestoreapi.com/products");
 
+  console.log(products);
+  products.forEach((element) => {
+    let li = document.createElement("li");
+    let img = document.createElement("img");
+    let rating = document.createElement("h4");
+    let titlee = document.createElement("h3");
+    let pricee = document.createElement("p");
+    let descriptionn = document.createElement("p");
+   
+    titlee.textContent = element.title;
+    img.setAttribute("src", element.image);
+    pricee.textContent = "$" + element.price;
+    descriptionn.textContent = element.description;
+    li.append(img, titlee, descriptionn, pricee);
 
-  let { data: products } = await axios.get(
-    "https://fakestoreapi.com/products",
-    {
-      image:img,
-      title:titlee,
-      price:pricee,
-      description:descriptionn,
-
-    },
-    
-    );
-
-
-    console.log(products);
-
-  
+    ul.append(li);
+  });
 });
-
-
